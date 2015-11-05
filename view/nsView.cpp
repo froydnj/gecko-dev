5013b4db5e0caf9615e25a9be24e67bea0d5e0bc
Nathan Froyd <froydnj@gmail.com>
2015-11-05 15:25:03 -0500

    maybe fix format string problem in nsView.cpp

--- view/nsView.cpp
+++ view/nsView.cpp
@@ -810,7 +810,7 @@ void nsView::List(FILE* out, int32_t aIndent) const
     nsrefcnt widgetRefCnt = mWindow.get()->AddRef() - 1;
     mWindow.get()->Release();
     int32_t Z = mWindow->GetZIndex();
-    fprintf(out, "(widget=%p[%" PRIuPTR "] z=%d pos={%d,%d,%d,%d}) ",
+    fprintf(out, "(widget=%p[%u] z=%d pos={%d,%d,%d,%d}) ",
             (void*)mWindow, widgetRefCnt, Z,
             nonclientBounds.x, nonclientBounds.y,
             windowBounds.width, windowBounds.height);
