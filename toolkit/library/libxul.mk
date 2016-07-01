324159c52e14a5a7e829f495f6ac8a59d949fa41
Nathan Froyd <froydnj@gmail.com>
2016-06-30 21:18:03 -0400

    force ICF on always

--- toolkit/library/libxul.mk
+++ toolkit/library/libxul.mk
@@ -16,6 +16,7 @@ LIBXUL_AUTOLOAD_FLAGS := -Dtopsrcdir=$(abspath $(topsrcdir))
 endif
 
 OS_LDFLAGS += -Wl,--gc-sections
+OS_LDFLAGS += -Wl,--icf=safe
 
 ifdef _MSC_VER
 get_first_and_last = dumpbin -exports $1 | grep _NSModule@@ | sort -k 3 | sed -n 's/^.*?\([^@]*\)@@.*$$/\1/;1p;$$p'
